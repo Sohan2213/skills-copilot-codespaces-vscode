1f847eca322dd83c5c8167149988f3b70d96b3ee
Sohan2213 <133603822+Sohan2213@users.noreply.github.com>
2023-07-20 23:02:51 +0530

    Update comments.js

--- comments.js
+++ comments.js
@@ -1,2 +1,18 @@
 // Create a web server
 Synthesizing 9/10 solutions (Duplicates hidden)
+
+Accept Solution
+const express = require('express');
+const router express. Router();
+
+const commentController = require('../controllers/comment
+
+router.get('/', commentController.comment_list);
+
+router.get('/create', commentController.comment_create_ge
+
+router.post('/create', commentController.comment_create_p
+
+router.get('/:id/delete', commentController.comment_delet
+git add comments.js
+git commit -m "Copilot third commit"
